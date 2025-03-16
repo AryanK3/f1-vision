98586baa0aec115352a20814ae18c5a4d3cafb54
AryanK3 <aryankiran136@gmail.com>
2025-03-16 00:03:15 -0400

    data

---
+++ convert_location_data.py
@@ -0,0 +1,21 @@
+import json
+
+with open('V1/driver_data.json', 'r') as file:
+    driver_data = json.load(file)
+
+events = []
+for driver in driver_data:
+    id = driver['id']
+    for pos in driver['positions']:
+        events.append({
+            "id": id,
+            "x": pos[0],
+            "y": pos[1],
+            "z": pos[2],
+            "rel_time": pos[3]
+        })
+
+events.sort(key=lambda x: x['rel_time'])
+
+with open("new_location_data.json", "w") as f:
+    json.dump(events, f)
